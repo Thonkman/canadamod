16c4b7a22e759bc8af785ece6fee327f26ae292d
Ioxom <jackwheppner@gmail.com>
2021-08-17 20:20:58 -0500

    get things mostly working TODO: documentation cleanup textures berry harvesting

--- src/main/java/canadamod/canadamod/block/plant/DoubleBerryBush.java
+++ src/main/java/canadamod/canadamod/block/plant/DoubleBerryBush.java
@@ -4,28 +4,22 @@ import canadamod.canadamod.registry.CanadamodItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Fertilizable;
-import net.minecraft.block.ShapeContext;
 import net.minecraft.block.TallPlantBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 import java.util.Random;
 
+//minecraft deprecates methods oddly
 @SuppressWarnings("deprecation")
 public class DoubleBerryBush extends TallPlantBlock implements Fertilizable {
     private static final IntProperty PLANT_AGE = IntProperty.of("plant_age", 0, 6);
@@ -33,18 +27,13 @@ public class DoubleBerryBush extends TallPlantBlock implements Fertilizable {
     private static final IntProperty BERRY_AGE = IntProperty.of("berry_age", 0, 3);
     private static final int MAX_BERRY_AGE = 3;
 
-    public DoubleBerryBush(String name, Settings settings, Item berryType, Item unripeBerryType, int sizeChangeAge, boolean spiky) {
+    public DoubleBerryBush(Settings settings) {
         super(settings);
     }
 
     @Override
     public ItemStack getPickStack(BlockView world, BlockPos pos, BlockState state) {
         return new ItemStack(CanadamodItems.SASKATOON_BERRIES);
-    }
-
-    @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return Block.createCuboidShape(16.0D, 16.0D, 16.0D, 16.0D, 16.0D, 16.0D);
     }
 
     @Override
@@ -58,13 +47,10 @@ public class DoubleBerryBush extends TallPlantBlock implements Fertilizable {
     }
 
     @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        return ActionResult.PASS;
-    }
-
-    @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(BERRY_AGE).add(PLANT_AGE);
+        //so it turns out I spent about an hour and a half debugging a crash before realising it originated from this property not existing
+        //yay me
+        builder.add(BERRY_AGE).add(PLANT_AGE).add(HALF);
     }
 
     @Override
@@ -76,7 +62,7 @@ public class DoubleBerryBush extends TallPlantBlock implements Fertilizable {
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         int age = state.get(BERRY_AGE);
         //if the age isn't maximum and the light level is high enough grow the bush
-        if (age <= MAX_BERRY_AGE && random.nextInt(5) == 0 && world.getBaseLightLevel(pos.up(), 0) >= 9) {
+        if (age < MAX_BERRY_AGE && random.nextInt(5) == 0 && world.getBaseLightLevel(pos.up(), 0) >= 9) {
             world.setBlockState(pos, state.with(BERRY_AGE, age + 1), 2);
         }
     }
--- src/main/java/canadamod/canadamod/block/plant/GrowingBerryBush.java
+++ src/main/java/canadamod/canadamod/block/plant/GrowingBerryBush.java
@@ -1,6 +1,7 @@
 package canadamod.canadamod.block.plant;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.TallPlantBlock;
 import net.minecraft.item.Item;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
@@ -9,8 +10,11 @@ import net.minecraft.util.shape.VoxelShape;
 import java.util.Random;
 
 public class GrowingBerryBush extends BasicBerryBush {
-    public GrowingBerryBush(String name, Settings settings, Item berryType, Item unripeBerryType, int maxBerryAge, VoxelShape smallShape, VoxelShape largeShape, int sizeChangeAge, boolean spiky) {
+    private final TallPlantBlock futureBush;
+
+    public GrowingBerryBush(String name, Settings settings, Item berryType, Item unripeBerryType, int maxBerryAge, VoxelShape smallShape, VoxelShape largeShape, int sizeChangeAge, boolean spiky, TallPlantBlock bush) {
         super(name, settings, berryType, unripeBerryType, maxBerryAge, smallShape, largeShape, sizeChangeAge, spiky);
+        this.futureBush = bush;
     }
 
     @Override
@@ -25,13 +29,7 @@ public class GrowingBerryBush extends BasicBerryBush {
     @Override
     public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
         int newAge = Math.min(maxBerryAge, state.get(BERRY_AGE) + 1);
-        System.out.println(newAge + " " + sizeChangeAge);
-        if (newAge < sizeChangeAge) {
-            world.setBlockState(pos, state.with(BERRY_AGE, newAge), 2);
-        } else {
-            System.out.println("growing");
-            world.setBlockState(pos, new DoubleBerryBush(name, settings, berryType, unripeBerryType, sizeChangeAge, spiky).getDefaultState(), 2);
-        }
+        grow(world, random, pos, state, newAge);
     }
 
     public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state, Integer newAge) {
@@ -41,9 +39,7 @@ public class GrowingBerryBush extends BasicBerryBush {
             if (newAge < sizeChangeAge) {
                 world.setBlockState(pos, state.with(BERRY_AGE, newAge), 2);
             } else {
-                System.out.println("growing");
-                //FIXME: crashes with exception: java.lang.IllegalArgumentException: Cannot set property EnumProperty{name=half, clazz=class net.minecraft.block.enums.DoubleBlockHalf, values=[upper, lower]} as it does not exist in Block{minecraft:air}
-                world.setBlockState(pos, new DoubleBerryBush(name, settings, berryType, unripeBerryType, sizeChangeAge, spiky).getDefaultState(), 2);
+                TallPlantBlock.placeAt(world, futureBush.getDefaultState(), pos, 2);
             }
         }
     }
